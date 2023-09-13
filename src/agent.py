b8ddc6c5bd4d8fce1cb464e2f579b06fd01bf1b9
frwd-1 <frwdlabs@gmail.com>
2023-09-13 16:58:29 -0600

    Create detection process

--- src/agent.py
+++ src/agent.py
@@ -1,8 +1,11 @@
-from forta_agent import transaction_event
 from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, and_
 from sqlalchemy.pool import QueuePool
 from networkx import Graph
-from community import best_partition  # This is for the Louvain method
+from community import best_partition  # Louvain method
+from sklearn.cluster import DBSCAN
+import numpy as np
+
+DATABASE_URL = "your_db_url_here"  # placeholder
 
 # Connection pooling
 engine = create_engine(DATABASE_URL, poolclass=QueuePool, pool_size=10)
@@ -29,7 +32,19 @@ async def analyze_transaction(transaction_event: transaction_event):
         )
 
 
-# Create graph and apply Louvain method
+def louvain_clustering(G):
+    return best_partition(G)
+
+
+def dbscan_clustering(G):
+    # Convert your graph into a format suitable for DBSCAN.
+    # This might involve creating an adjacency matrix or some similar representation.
+    # Assuming `matrix` is your adjacency matrix:
+    matrix = np.array(nx.to_numpy_matrix(G))
+    clustering = DBSCAN(eps=0.5, min_samples=2).fit(matrix)
+    return clustering.labels_
+
+
 def process_clusters():
     G = Graph()
 
@@ -39,14 +54,41 @@ def process_clusters():
             G.add_edge(row["sender"], row["receiver"], weight=row["amount"])
 
     # Apply Louvain
-    partitions = best_partition(G)
+    partitions_louvain = louvain_clustering(G)
 
-    # For now, just print clusters; can be stored or further processed
-    for node, cluster_id in partitions.items():
-        print(f"Node {node} belongs to cluster {cluster_id}")
+    # Apply DBSCAN
+    partitions_dbscan = dbscan_clustering(G)
 
-    # Optional: Prune unrelated transactions
-    unrelated_txs = []  # logic to determine these
-    with engine.connect() as conn:
-        for tx in unrelated_txs:
-            conn.execute(transactions.delete().where(transactions.c.tx_hash == tx))
+    # Decision mechanism (you could intersect, union, or have some other mechanism)
+    # This is a simple example where we just print out both results.
+    # In a real scenario, you'd want a more sophisticated way to merge or choose between them.
+    for node, cluster_id in partitions_louvain.items():
+        print(f"[Louvain] Node {node} belongs to cluster {cluster_id}")
+
+    for index, cluster_id in enumerate(partitions_dbscan):
+        node = G.nodes()[index]
+        print(f"[DBSCAN] Node {node} belongs to cluster {cluster_id}")
+
+
+# old.....
+# Create graph and apply Louvain method
+# def process_clusters():
+#     G = Graph()
+
+#     # Load data from DB into the graph
+#     with engine.connect() as conn:
+#         for row in conn.execute(transactions.select()):
+#             G.add_edge(row["sender"], row["receiver"], weight=row["amount"])
+
+#     # Apply Louvain
+#     partitions = best_partition(G)
+
+#     # For now, just print clusters; can be stored or further processed
+#     for node, cluster_id in partitions.items():
+#         print(f"Node {node} belongs to cluster {cluster_id}")
+
+#     # Optional: Prune unrelated transactions
+#     unrelated_txs = []  # logic to determine these
+#     with engine.connect() as conn:
+#         for tx in unrelated_txs:
+#             conn.execute(transactions.delete().where(transactions.c.tx_hash == tx))
