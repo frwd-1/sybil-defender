3527e4831c1889afad344b77f0ff9b9c755f2b48
frwd-1 <frwdlabs@gmail.com>
2023-10-23 17:58:48 -0600

    Updates and testing

--- src/alerts/cluster_alerts.py
+++ src/alerts/cluster_alerts.py
@@ -1,38 +1,76 @@
 from Crypto.Hash import keccak
 from forta_agent import Finding, FindingSeverity, FindingType, EntityType
 
+import json
 
-def generate_alert_details(community_id, nodes, labels, contracts):
+
+def extract_addresses(entity):
+    # Remove leading/trailing whitespaces and the characters "[", "]"
+    cleaned_string = entity.strip().strip("[").strip("]").replace('"', "")
+
+    # Split the addresses based on comma separation
+    addresses = cleaned_string.split(",")
+
+    # Ensure each address is properly formatted (e.g., starts with "0x")
+    return [format_address(address.strip()) for address in addresses]
+
+
+def format_address(address):
+    """Ensure the Ethereum address starts with '0x'."""
+    if not address.startswith("0x"):
+        return "0x" + address
+    return address
+
+
+def generate_alert_details(community_id, nodes, labels, contracts, action):
+    # Preparing metadata that includes details about the cluster and contracts.
+    if contracts and isinstance(contracts, str):
+        # Assuming 'contracts' is the string containing the list representation
+        contracts = extract_addresses(contracts)
+
+    metadata = {
+        "cluster_id": community_id,
+        "interacted_contracts": list(
+            contracts
+        ),  # Contracts are now only listed in the metadata.
+    }
+
+    # Constructing labels for nodes, each enriched with metadata.
+    labels_with_metadata = [
+        {
+            "entity_type": EntityType.Address.value,
+            "entity": node,
+            "confidence": 90,
+            "label": label,
+            "metadata": metadata,  # Attaching metadata to each node's label.
+        }
+        for node in nodes
+        for label in labels
+    ]
+
+    # Depending on the action, we set different alert names and descriptions.
+    if action == "created":
+        alert_name = "New sybil asset farmer detected"
+        alert_description = f"Cluster {community_id} shows signs of sybil asset farming"
+    else:  # For "updated" or any other action
+        alert_name = "Existing sybil cluster updated"
+        alert_description = (
+            f"Cluster {community_id} has been updated with new activities"
+        )
+
+    # Assembling the final structure of the alert details.
     alert_details = {
-        "name": "Sybil asset farmer detected",
-        "description": f"Cluster {community_id} shows signs of sybil asset farming",
+        "name": alert_name,
+        "description": alert_description,
         "alert_id": keccak.new(
-            data=f"sybil_asset_farmer_{community_id}".encode(),
-            digest_bits=256,
+            data=f"sybil_asset_farmer_{community_id}".encode(), digest_bits=256
         ).hexdigest(),
-        "severity": FindingSeverity.Medium,
+        "severity": FindingSeverity.High,
         "type": FindingType.Suspicious,
         "addresses": list(nodes),
-        "labels": [
-            {
-                "entity_type": EntityType.Address.value,
-                "entity": node,
-                "confidence": 90,
-                "label": label,
-            }
-            for node in nodes
-            for label in labels
-        ]
-        + [
-            {
-                "entity_type": EntityType.Address.value,
-                "entity": contract,
-                "confidence": 90,
-                "label": "interacted_by_sybil",
-            }
-            for contract in contracts
-        ],
+        "labels": labels_with_metadata,  # Using the constructed labels with metadata.
     }
+
     return alert_details
 
 
--- src/database/clustering.py
+++ src/database/clustering.py
@@ -10,23 +10,31 @@ from src.alerts.cluster_alerts import generate_alert_details
 from forta_agent import Finding
 
 
+import asyncio
+
+
 async def write_graph_to_database(final_graph):
+    print("Function 'write_graph_to_database' started.")
     findings = []
 
     async with get_async_session() as session:
-        # Load all existing nodes from the database for comparison
+        print("Opened database session.")
+
+        # Fetch existing nodes and communities.
         existing_nodes = await load_all_nodes_from_database(session)
+        print(f"Loaded {len(existing_nodes)} nodes from database.")
 
-        # Store updated clusters and their nodes
         updated_clusters = {}
+        new_communities = set()
 
-        # Iterate and filter through graph nodes with 'label' attribute
         for node, data in final_graph.nodes(data=True):
+            print(f"Processing node: {node}")
+
             if "label" in data:
                 label = data["label"]
                 community_id = data.get("community")
+                print(f"Found label: {label}, community_id: {community_id}")
 
-                # Convert interacting_contracts from list to string
                 if "interacting_contracts" in data and isinstance(
                     data["interacting_contracts"], list
                 ):
@@ -34,40 +42,55 @@ async def write_graph_to_database(final_graph):
                         data["interacting_contracts"]
                     )
 
-                # Check if node already exists
                 existing_node = existing_nodes.get(node)
 
-                # If new labeled node or label has changed, add to updated_clusters
-                if not existing_node or existing_node.labels != label:
+                if not existing_node:
+                    # Node doesn't exist, so this could be a new community or an update to an existing one.
                     if community_id not in updated_clusters:
+                        # This is a new community or the first instance of an update to an existing community.
                         updated_clusters[community_id] = {
                             "nodes": set(),
                             "labels": set(),
                             "contracts": set(),
                         }
+                        # If the community_id doesn't exist in the database, it's a new community.
+                        if (
+                            community_id not in existing_nodes.values()
+                        ):  # Assuming existing_nodes.values() gives existing community_ids.
+                            new_communities.add(community_id)
+
                     updated_clusters[community_id]["nodes"].add(node)
                     updated_clusters[community_id]["labels"].add(label)
-
-                    # Update contracts; ensure we split the string back into a list for processing
                     updated_clusters[community_id]["contracts"].update(
                         data.get("interacting_contracts", "").split(",")
                     )
 
-                    # Insert or update in database
                     new_cluster = SybilClusters(
                         cluster_id=str(community_id), address=node, labels=label
                     )
                     await session.merge(new_cluster)
+                    print(f"Node {node} merged into session.")
 
         await session.commit()
+        print("Session commit completed.")
 
-        # Generate findings for updated clusters
         for community_id, data in updated_clusters.items():
+            if community_id in new_communities:
+                action = "created"
+            else:
+                action = "updated"
+
             alert_details = generate_alert_details(
-                community_id, data["nodes"], data["labels"], data["contracts"]
+                community_id,
+                data["nodes"],
+                data["labels"],
+                data["contracts"],
+                action=action,
             )
             findings.append(Finding(alert_details))
+            print(f"Alert generated for {action} community: {community_id}")
 
+    print("Function 'write_graph_to_database' completed.")
     return findings
 
 
